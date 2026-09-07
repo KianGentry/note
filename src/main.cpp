a86f17515567b6848368dcbbe98e2f029e84ee00
Kian Gentry <musinks@pm.me>
2026-09-07 12:44:48 +0100

    main: add help ver and temporary constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+const char* VERSION = "1";
+std::string tmp_name = "/tmp/note_";
+
+void print_help() {
+    std::cout << "Usage: program [options]\n";
+    std::cout << "Options:\n";
+    std::cout << "  -h, --help     Show this help message\n";
+    std::cout << "  -v, --version  Show version information\n";
+}
+
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!\n";
+    if (argc < 2 || std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help") {
+        print_help();
+    }
+    else if (std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version") {
+        std::cout << "Program version " << VERSION << "\n";
+    } 
+    else {
+        std::cout << "Unknown option: " << argv[1] << "\n";
+        print_help();
+    }
     return 0;
 }

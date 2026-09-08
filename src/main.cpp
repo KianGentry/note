3742c31a51b4e36c526250b3752a6bf9ba97749e
Kian Gentry <musinks@pm.me>
2026-09-08 03:01:28 +0100

    main: add list_notes (-l)

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 const char* VERSION = "1";
 std::string tmp_name = "/tmp/note_";
 
 void print_help() {
-    std::cout << "Usage: program [options]\n";
+    std::cout << "Usage: note [OPTION]\n";
     std::cout << "Options:\n";
     std::cout << "  -h, --help     Show this help message\n";
     std::cout << "  -v, --version  Show version information\n";
+    std::cout << "  -l, --list     List all notes\n";
+}
+
+void list_notes() {
+    std::system("ls -a /tmp | grep note_");
 }
 
 int main(int argc, char* argv[]) {
@@ -17,6 +24,9 @@ int main(int argc, char* argv[]) {
     else if (std::string(argv[1]) == "-v" || std::string(argv[1]) == "--version") {
         std::cout << "Program version " << VERSION << "\n";
     } 
+    else if (std::string(argv[1]) == "-l" || std::string(argv[1]) == "--list") {
+        list_notes();
+    }
     else {
         std::cout << "Unknown option: " << argv[1] << "\n";
         print_help();
